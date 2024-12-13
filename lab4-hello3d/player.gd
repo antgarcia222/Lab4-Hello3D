a0954709c2541711b9b3106c31ae824d8bfee9b5
antgarcia222 <agarcia222@fordham.edu>
2024-12-13 01:47:11 -0500

    Polished the camera moving
    Finalized the player's camera movements.
    
    The player now moves respective to the camera angle.

--- lab4-hello3d/player.gd
+++ lab4-hello3d/player.gd
@@ -4,6 +4,10 @@ var mouse_sensitivity := 0.001
 var twist_input := 0.0
 var pitch_input := 0.0
 
+@onready var twist_pivot := $TwistPivot
+@onready var pitch_pivot := $TwistPivot/PitchPivot
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -16,21 +20,19 @@ func _process(delta: float) -> void:
 	input.x = Input.get_axis("move_left", "move_right")
 	input.z = Input.get_axis("move_forward", "move_back")
 	
-	apply_central_force(input * 1200.0 * delta)
+	apply_central_force(twist_pivot.basis * input * 1200.0 * delta)
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	$TwistPivot.rotate_y(twist_input)
-	$TwistPivot/PitchPivot.rotate_x(pitch_input)
-	$TwistPivot/PitchPivot.rotation.x = clamp (
-		$TwistPivot/PitchPivot.rotation.x,
-		-0.5,
-		0.5
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp (pitch_pivot.rotation.x, 
+		deg_to_rad(-30), 
+		deg_to_rad(30)
 	)
 	twist_input = 0.0
 	pitch_input = 0.0
-	
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
